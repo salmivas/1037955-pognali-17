5e0a67556820c5e42fd8a95a3aacead6ea2e976b
Ivan Salmin <salmin.iv@gmail.com>
2019-09-06 22:31:44 +0400

    Completes pixel perfect before checking crossbrowsers compliance

--- source/js/menu_open.js
+++ source/js/menu_open.js
@@ -2,18 +2,18 @@ var pageHeader = document.querySelector('.page-header');
 var pageToggle = document.querySelector('.page-header__toggle');
 var pageMenuToggle = document.querySelector('.page-header__menu-toggle');
 
-pageHeader.classList.remove('page-header__menu--nojs');
+pageHeader.classList.remove('page-header--nojs');
 
 pageToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header__menu--closed')) {
-    pageHeader.classList.remove('page-header__menu--closed');
-    pageHeader.classList.add('page-header__menu--opened');
+  if (pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
   }
 });
 
 pageMenuToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header__menu--opened')) {
-    pageHeader.classList.remove('page-header__menu--opened');
-    pageHeader.classList.add('page-header__menu--closed');
+  if (pageHeader.classList.contains('page-header--opened')) {
+    pageHeader.classList.remove('page-header--opened');
+    pageHeader.classList.add('page-header--closed');
   }
 });
